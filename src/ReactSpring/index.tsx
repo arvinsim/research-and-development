f384b1d0e6a2888753f09a6c136f8f065d0ef500
Arthur Vincent Simon <developarvin@gmail.com>
2021-04-26 15:50:38 +0800

    Update react animation

--- src/ReactSpring/index.tsx
+++ src/ReactSpring/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSpring, animated, config } from "react-spring";
 
 export function ReactSpring() {
@@ -18,9 +18,18 @@ function YourNetWorth() {
     config: config.default,
   });
 
+  const h1Props = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    reset: true,
+    delay: 400,
+    config: config.molasses,
+  });
+
   return (
-    <div>
-      Your net worth is now <animated.span>{number.to((n) => n.toFixed(2))}</animated.span>
-    </div>
+    <animated.h1 style={h1Props}>
+      Your net worth is now{" "}
+      <animated.span>{number.to((n) => n.toFixed(2))}</animated.span>
+    </animated.h1>
   );
 }
