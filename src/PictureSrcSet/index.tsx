d216a58f027d29895153dc6bf020e0da69c5d9bc
Arthur Vincent Simon <developarvin@gmail.com>
2021-07-07 11:12:51 +0800

    Implement image import

--- src/PictureSrcSet/index.tsx
+++ src/PictureSrcSet/index.tsx
@@ -1,29 +1,16 @@
 import React from "react";
+import Puppy200w from "./puppy-200w.png";
+import Puppy800w from "./puppy-800w.png";
 
 function PictureSrcSet() {
   return (
     <div>
-      <div>
-        <h1>SrcSet</h1>
-        <img
-          width="800"
-          height="533"
-          src="./puppy-800w.png"
-          srcSet="./puppy-480w.png 480w, ./puppy-800w.png 800w"
-          alt="SrcSet Puppy with balloons"
-        />
-      </div>
-      <div>
-        <h1>Picture</h1>
-        <picture>
-          <source media="(max-width: 799px)" srcSet="./puppy-480w.png" />
-          <source media="(min-width: 800px)" srcSet="./puppy-800w.png" />
-          <img
-            src="/src/PictureSrcSet/puppy-800w.png"
-            alt="Picture Puppy with balloons"
-          />
-        </picture>
-      </div>
+      <div>Picture</div>
+      <picture>
+        <source media="(max-width: 376px)" srcSet={Puppy200w} />
+        <source media="(min-width: 480px)" srcSet={Puppy800w} />
+        <img src={Puppy800w} alt="Puppy" />
+      </picture>
     </div>
   );
 }
