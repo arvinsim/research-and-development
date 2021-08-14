bc02225edbd2f960539d6668ae898e0dce7f29ae
Arthur Vincent Simon <developarvin@gmail.com>
2021-08-14 11:06:16 +0800

    Implement Streaming page

--- src/Streaming/index.tsx
+++ src/Streaming/index.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 
 function Streaming() {
-  return <div>Streaming</div>;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoPlayer = useRef(null);
+
+  // This is a PoC for custom playback
+  const handleClick = () => {
+    if (videoPlayer.current === null) {
+      return;
+    }
+
+    if (isPlaying) {
+      videoPlayer.current.pause();
+      setIsPlaying(false);
+    } else {
+      videoPlayer.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <div onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</div>
+      </div>
+      <video
+        id="video1"
+        width="1280px"
+        height="720px"
+        ref={videoPlayer}
+        playsInline
+        muted
+        controls
+      >
+        <source src="http://localhost:8000/videostream" type="video/mp4" />
+        Your browser does not support HTML video.
+      </video>
+    </div>
+  );
 }
 
 export default Streaming;
