1bd93cda71825bd3cd104f007485a3fea0ed523b
Arthur Vincent Simon <developarvin@gmail.com>
2021-08-19 13:48:07 +0800

    Add markers

--- src/PhotographyMap/index.tsx
+++ src/PhotographyMap/index.tsx
@@ -4,8 +4,27 @@ function PhotographyMap() {
   return (
     <div>
       <h1>Photography Map</h1>
+      <div id="photography-map"></div>
     </div>
   );
 }
+
+const locationsMarkers = [
+  {
+    name: "Labrador Park",
+    lat: 1.2667528884241017,
+    long: 103.802820878488,
+  },
+  {
+    name: "Marina Barrage",
+    lat: 1.281119063584918,
+    long: 103.87410672901598,
+  },
+  {
+    name: "Lower Peirce Reservoir",
+    lat: 1.3702173441083043,
+    long: 103.82572641732168,
+  },
+];
 
 export default PhotographyMap;
